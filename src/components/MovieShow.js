32300a427f1b4b375106ac96473891fcb5673a72
Noelle <noelle.maingi@student.moringaschool.com>
2023-07-27 13:43:20 +0300

    Completed Assignment

---
+++ src/components/MovieShow.js
@@ -0,0 +1,15 @@
+import { useParams } from "react-router-dom"
+
+function MovieShow ({movies}) {
+    const params = useParams();
+    console.log(params);
+
+    const movie = movies.find(movie => movie.id === parseInt(params.movieId))
+return (
+    <div>
+        <h3>{movie.title}</h3>
+    </div>
+)
+}
+
+export default MovieShow
